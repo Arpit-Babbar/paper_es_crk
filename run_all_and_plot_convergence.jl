52107c3074371ce58b2d001f335ca7e79942def8
Arpit Babbar <arpitbabbar@gmail.com>
2026-04-02 15:55:47 +0200

    Fixes

--- run_all_and_plot_convergence.jl
+++ run_all_and_plot_convergence.jl
@@ -2,14 +2,14 @@ import Pkg
 Pkg.activate(joinpath(@__DIR__, ".."))
 
 # Convergence tests
-include(joinpath(@__DIR__, "convergence.jl")) # Isentropic and multi-ion convergence test
-include(joinpath(@__DIR__, "plot_convergence.jl")) # Plotting convergence results
+include(joinpath(@__DIR__, "run", "convergence.jl")) # Isentropic and multi-ion convergence test
+include(joinpath(@__DIR__, "run", "plot_convergence.jl")) # Plotting convergence results
 
 # KHI test case from Chan et al. 2022
-include(joinpath(@__DIR__, "run_khi_chan2022.jl"))
+include(joinpath(@__DIR__, "run", "run_khi_chan2022.jl"))
 
 # Richmyer-Meshkov instability test case
-include(joinpath(@__DIR__, "run_richtmeyer_meshkov_chan.jl"))
+include(joinpath(@__DIR__, "run", "run_richtmeyer_meshkov_chan.jl"))
 
 # Multi-ion KHI
-include(joinpath(@__DIR__, "run_multiion_khi_crk_es.jl"))
+include(joinpath(@__DIR__, "run", "run_multiion_khi_crk_es.jl"))
